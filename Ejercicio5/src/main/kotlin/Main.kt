b05e1138dff963d495aa24bce03d11b8eedc2a55
lnarote777 <nolara@iesrafaelalberti.es>
2024-01-25 21:33:32 +0100

    Ejercicio5

--- Ejercicio5/src/main/kotlin/Main.kt
+++ Ejercicio5/src/main/kotlin/Main.kt
@@ -1,7 +1,23 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    try {
+        println("Introduzca la hora:")
+        val hora = readln().toInt()
+
+        println("Introduzca los minutos:")
+        val minutos = readln().toIntOrNull()
+
+        println("Introduzca los segundos")
+        val segundos = readln().toIntOrNull()
+
+        if (minutos != null || segundos != null)
+
+        val tiempo = Tiempo(hora, minutos, segundos)
+
+        print(tiempo.toString())
+    }catch (e: Exception){
+        print("**ERROR** Caracter no válido. Debe ser un número entero")
+    }
+
+
 }
